30251daa69e3ea881ca9e61c3f0de19520b18ddd
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2025-06-01 20:15:21 +0300

    data loc fix

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const data = require('./data.json');
+const data = require('./data/data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
