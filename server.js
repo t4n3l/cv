dba66972b0c42bc223ab972f2816d6e7c2b50981
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2025-06-01 19:20:46 +0300

    ..

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.get('/', (request, response) => {
-    
     const jsonFilePath = path.join(__dirname, 'data', 'data.json');
-
     fs.readFile(jsonFilePath, (error, dataFromFile) => {
         if(error) {
             console.error('Failed to read from file', error);
             return response.status(500).send('Failed to read from file.');
         }
-
         const jsonData = JSON.parse(dataFromFile);
         response.render('index', {recipe: jsonData} );
-        
-
     })
-
 });
-
-
 app.listen(PORT, () => {
     console.log('Server is running on port: ' + PORT);
 });
